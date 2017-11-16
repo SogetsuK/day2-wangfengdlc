795294a2787a6002e35c055616c4e21fbb086ba4
SogetsuK <33680195+SogetsuK@users.noreply.github.com>
2017-11-16 17:25:00 +0900

    GetOpt代码

---
+++ src/GetOptDemoNew.java
@@ -0,0 +1,46 @@
+import java.util.Map;
+
+import com.darwinsys.lang.GetOpt;
+import com.darwinsys.lang.GetOptDesc;
+
+public class GetOptDemoNew {
+	public static void main(String[] argv) {
+		boolean numeric_option = true;
+		boolean errs = true;
+		String outputFileName = null;
+
+		GetOptDesc[] options = {
+				new GetOptDesc('n', "numeric", false),
+				new GetOptDesc('o', "output-file", false)
+		};
+		GetOpt parser = new GetOpt(options);
+		Map<String,String>optionsFound = parser.parseArguments(argv);
+		for(String key : optionsFound.keySet()) {
+			char c = key.charAt(0);   // 查找第一位
+			switch(c) {
+			case 'n' :
+				numeric_option = true;
+				break;
+			case'o' :
+				outputFileName =(String)optionsFound.get(key);
+			case'?' :
+				errs = true;
+				break;
+			default :
+				throw new IllegalStateException("Unexpected option character" + c);
+			}
+		}
+		if(errs) {
+			System.out.println("Usage: GetOptDemo [-n][-o file][file...]");
+		}
+		System.out.print("Options: ");
+		System.out.print("Numeric: " + numeric_option + ' ');
+		System.out.print("Output: " + outputFileName + ";");
+		System.out.print("Input files: ");
+		for(String fileName : parser.getFilenameList()) {
+			System.out.print(fileName);
+			System.out.print(' ');
+		}
+		System.out.println();
+	}
+}
